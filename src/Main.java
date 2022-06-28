260e3ec93512b87e129a0140ca6337b5dd75d9fa
Andrei Nikanorov <flea07one@yandex.ru>
2022-06-28 16:17:00 +0300

    initital commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 190;
+        int milePrice = 20;
+        int miles = amount / milePrice;
+
+        System.out.println("Вам будет начислено " + miles + " бонусных миль");
+    }
+}
